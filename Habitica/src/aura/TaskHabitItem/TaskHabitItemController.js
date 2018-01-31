2e940cd1834b683801d3cc69816b7ad138872cc7
Yandris Mata <ymata@altimetrik.com>
2018-01-31 17:18:44 -0300

    Revert "Changes related with the part 2 of the Habitica project."
    This reverts commit 130a60cb9f31fbfed84d3512f897fbe7518c23cb.

--- Habitica/src/aura/TaskHabitItem/TaskHabitItemController.js
+++
@@ -1,8 +0,0 @@
-({
-	fireEventOpenTaskUpdateWindow: function(component, event, helper) {
-        var taskItem = component.get("v.item");
-        var showTaskUpdateWindowEvent = component.getEvent("showTaskUpdateWindow");
-        showTaskUpdateWindowEvent.setParams({ "taskItem": taskItem });
-        showTaskUpdateWindowEvent.fire();
-    },
-})
